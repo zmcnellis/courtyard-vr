93c69886cf7cb4cdd3f1551e674faa75abb55e6e
Zachary McNellis <zmcnell@g.clemson.edu>
2019-04-01 19:56:20 -0500

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import Analytics from 'react-router-ga'
 import Routes from './routes'
 import './index.css'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes />
+    <Analytics id='UA-137462836-1'>
+      <Routes />
+    </Analytics>
   </BrowserRouter>,
   document.getElementById('root')
 )
